d8a796aec2d0e3decab246575f311387fbeda265
Hugo <hugoluque19102004@gmail.com>
2023-02-07 23:40:24 +0100

    Creating app react

---
+++ src/Component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Component() {
+
+    
+
+    return (
+        <div>
+            <input type="text" />
+            <button>actulizar</button>
+            <p>Texto input</p>
+            <p>Texto actualizado</p>
+        </div>
+    )
+}
