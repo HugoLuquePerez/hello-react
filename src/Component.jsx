49294f14821a41cb8aabbb06a2f6445f1b975bae
Hugo <hugoluque19102004@gmail.com>
2023-02-08 00:20:02 +0100

    terminado

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Component() {
 
+    const [text, setText] = useState()
+    const [update, setUpdate] = useState()
     
+    const textOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const buttoOnClick = () => {
+        setUpdate(text)
+    }
 
     return (
         <div>
-            <input type="text" />
-            <button>actulizar</button>
-            <p>Texto input</p>
-            <p>Texto actualizado</p>
+            <input type="text" value={text} onChange={textOnChange} />
+            <button onClick={buttoOnClick}>Actualizar</button>
+            <p>Texto input: {text}</p>
+            <p>Texto actualizado {update}</p>
         </div>
     )
 }
